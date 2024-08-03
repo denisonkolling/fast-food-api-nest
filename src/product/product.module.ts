b8fb332bea16a6ee3dfccedfa4822109add636b0
Denison Kolling <denisonkolling@gmail.com>
2024-08-03 06:27:34 -0300

    chore: update module exports

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -8,5 +8,6 @@ import { Product } from './entities/product.entity';
   imports: [MikroOrmModule.forFeature([Product])],
   controllers: [ProductController],
   providers: [ProductService],
+  exports: [ProductService],
 })
 export class ProductModule {}
