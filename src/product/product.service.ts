45e76890f8c68eb3d653e845ebe7b94f1bfa7539
Denison Kolling <denisonkolling@gmail.com>
2024-08-03 21:25:01 -0300

    refactor: update from product repository to entity manager

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -2,28 +2,27 @@ import { Injectable } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { Product } from './entities/product.entity';
-import { InjectRepository } from '@mikro-orm/nestjs';
-import { EntityRepository } from '@mikro-orm/core';
+import { EntityManager } from '@mikro-orm/postgresql';
 
 @Injectable()
 export class ProductService {
   constructor(
-    @InjectRepository(Product)
-    private readonly productRepository: EntityRepository<Product>,
+    private readonly entityManager: EntityManager,
   ) {}
 
   async create(createProductDto: CreateProductDto) {
-    const product = this.productRepository.create(createProductDto);
-    await this.productRepository.insert(product);
+    const product = new Product();
+    this.entityManager.assign(product, createProductDto);
+    await this.entityManager.persist(product).flush();
     return product;
   }
 
   findAll() {
-    return this.productRepository.findAll();
+    return this.entityManager.findAll(Product);
   }
 
   async findOne(id: number): Promise<Product> {
-    const product = await this.productRepository.findOne(id);
+    const product = await this.entityManager.findOne(Product, id);
 
     if (!product) {
       throw new Error('Product not found');
@@ -32,13 +31,13 @@ export class ProductService {
     return product;
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return this.productRepository.nativeUpdate({ id }, updateProductDto);
+  async update(id: number, updateProductDto: UpdateProductDto): Promise<number> {
+    const updatedProduct = await this.entityManager.nativeUpdate(Product, { id }, updateProductDto);
+    return updatedProduct;
   }
 
   async remove(id: number) {
-    
-    const product = await this.productRepository.findOne(id);
+    const product = await this.entityManager.findOne(Product, id);
 
     if (!product) {
       throw new Error('User not found');
@@ -46,8 +45,14 @@ export class ProductService {
 
     product.isDeleted = true;
 
-    await this.productRepository.nativeUpdate({ id }, product);
+    this.updateTimestamp(product);
+
+    await this.entityManager.nativeUpdate(Product, { id }, product);
 
     return product;
+  }
+
+  private updateTimestamp(product: Product): void {
+    product.updatedAt = new Date();
   }
 }
