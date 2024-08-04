9e649cb8c40887d8fb5e8e15fbcc22bc538086d1
Denison Kolling <denisonkolling@gmail.com>
2024-08-04 19:50:07 -0300

    feat: add user validations

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,4 +1,37 @@
-import { PickType } from '@nestjs/mapped-types';
+import {
+  IsEmail,
+  IsPositive,
+  IsString,
+  Matches,
+  matches,
+  Max,
+  MaxLength,
+  maxLength,
+  MinLength,
+} from 'class-validator';
 import { User } from '../entities/user.entity';
 
-export class CreateUserDto extends PickType(User, ['username']) {}
+export class CreateUserDto extends User {
+  @IsString()
+  firstName!: string;
+
+  @IsString()
+  lastName!: string;
+
+  @IsString()
+  username!: string;
+
+  @IsPositive()
+  document!: number;
+
+  @IsEmail()
+  email!: string;
+
+  @IsString()
+  @MinLength(8)
+  @MaxLength(20)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'Password to weak: chose a stroger one!',
+  })
+  password: string;
+}
