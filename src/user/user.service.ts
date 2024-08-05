ddfa73e78d85d190f094838a5a495609ac85291b
Denison Kolling <denisonkolling@gmail.com>
2024-08-05 13:24:46 -0300

    feat: implement user password bcrypt hash

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,33 +1,52 @@
-//import { type EntityRepository } from '@mikro-orm/postgresql'; when using query builder
-import { EntityRepository } from '@mikro-orm/core';
+import { EntityManager, EntityRepository } from '@mikro-orm/core';
 import { InjectRepository } from '@mikro-orm/nestjs';
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './entities/user.entity';
+import { hashPassword } from '../common/helpers';
 
 @Injectable()
 export class UserService {
   constructor(
     @InjectRepository(User)
     private readonly userRepository: EntityRepository<User>,
+    private readonly entityManager: EntityManager,
   ) {}
 
-  async create(createUserDto: CreateUserDto) {
-    const user = this.userRepository.create(createUserDto);
-    await this.userRepository.insert(user);
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    const user = new User();
+
+    const hashedPassword = await hashPassword(createUserDto.password);
+
+    this.entityManager.assign(user, {
+      ...createUserDto,
+      password: hashedPassword,
+    });
+
+    await this.entityManager.persistAndFlush(user);
     return user;
   }
 
-  findAll() {
-    return this.userRepository.findAll();
+  async findAll() {
+    return await this.userRepository.findAll();
   }
 
   async findOne(id: number): Promise<User> {
     const user = await this.userRepository.findOne(id);
 
     if (!user) {
-      throw new Error('User not found');
+      throw new NotFoundException('User not found');
+    }
+
+    return user;
+  }
+
+  async findByEmail(email: string): Promise<User> {
+    const user = await this.userRepository.findOne({ email });
+
+    if (!user) {
+      throw new NotFoundException('User not found');
     }
 
     return user;
@@ -38,7 +57,6 @@ export class UserService {
   }
 
   async remove(id: number) {
-    
     const user = await this.userRepository.findOne(id);
 
     if (!user) {
